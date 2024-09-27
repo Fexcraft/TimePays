bf777893a244d0230d2f9f447cf7ac55243e344f
Ferdinand Calo <fexcalo@gmail.com>
2024-09-27 17:17:27 +0200

    Update Config.java

--- src/main/java/net/fexcraft/mod/tpm/Config.java
+++ src/main/java/net/fexcraft/mod/tpm/Config.java
@@ -3,58 +3,62 @@ package net.fexcraft.mod.tpm;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.app.json.JsonValue;
 import net.fexcraft.lib.common.json.JsonUtil;
 import net.fexcraft.mod.tpm.compat.RewardHandler;
+import net.fexcraft.mod.uni.ConfigBase;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.config.IConfigElement;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class Config {
+public class Config extends ConfigBase {
 	
 	public static long INTERVAL;
-	//
-	public static final String CATEGORY = "Settings";
-	private static Configuration config;
-	
-	public static void initialize(FMLPreInitializationEvent event){
-		File file = new File(event.getSuggestedConfigurationFile().getParentFile() + "/tpm_rewards.json");
-		if(!file.getParentFile().exists()) file.getParentFile().mkdirs(); if(!file.exists()) JsonUtil.write(file, new JsonArray());
-		JsonElement arrey = JsonUtil.read(file, false); JsonArray array = arrey.getAsJsonArray();
-		for(JsonElement elm : array){
-			try{
-				Reward reward = new Reward(elm.getAsJsonObject());
-				RewardHandler.REWARDS.put(reward.id, reward);
-			}
-			catch(Exception e){
-				e.printStackTrace();
-			}
-		}
-		//
-		config = new Configuration(event.getSuggestedConfigurationFile(), "1.0", true);
-		config.load();
-		config.setCategoryRequiresMcRestart(CATEGORY, true);
-		config.setCategoryRequiresWorldRestart(CATEGORY, true);
-		config.setCategoryComment(CATEGORY, "General Settings.");
-		refresh(); config.save();
+
+	public Config(File fl){
+		super(fl);
 	}
 
-	public static List<IConfigElement> getList(){
-		List<IConfigElement> list = new ArrayList<IConfigElement>();
-		list.add(new ConfigElement(Config.getConfig().getCategory(CATEGORY)));
-		return list;
+	@Override
+	protected void fillInfo(JsonMap map){
+		map.add("info", "TimePays Configuration File");
+		map.add("wiki", "https://fexcraft.net/wiki/mod/timepays");
 	}
 
-	public static final Configuration getConfig(){
-		return config;
+	@Override
+	protected void fillEntries(){
+		entries.add(new ConfigEntry(this, "general", "interval", 1).rang(1, 1440)
+			.info("Interval in which player activity is checked, in minutes.")
+			.cons((con, map) -> INTERVAL = con.getInteger(map))
+		);
+		entries.add(new ConfigEntry(this, "general", "rewards", new JsonMap())
+			.info("Map with Reward entries.")
+			.cons((con, map) -> {
+				RewardHandler.REWARDS.clear();
+				JsonMap rwm = con.getJson(map).asMap();
+				for(Map.Entry<String, JsonValue<?>> entry : rwm.entries()){
+					try{
+						Reward reward = new Reward(entry.getKey(), entry.getValue().asMap());
+						RewardHandler.REWARDS.put(reward.id, reward);
+					}
+					catch(Exception e){
+						e.printStackTrace();
+					}
+				}
+			})
+		);
 	}
-	
-	private static void refresh(){
-		INTERVAL = config.getInt("interval", CATEGORY, 1, 1, 1440, "Interval in which player activity is checked, in minutes.");
+
+	@Override
+	protected void onReload(JsonMap map){
+
 	}
-	
+
 }
