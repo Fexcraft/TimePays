27b62cb65718576e352bd081dd18c6b4644b9bf4
Ferdinand Calo <fexcalo@gmail.com>
2024-09-28 10:10:08 +0200

    Update RewardHandler.java

--- src/main/java/net/fexcraft/mod/tpm/compat/RewardHandler.java
+++ src/main/java/net/fexcraft/mod/tpm/compat/RewardHandler.java
@@ -3,13 +3,13 @@ package net.fexcraft.mod.tpm.compat;
 import java.util.TreeMap;
 
 import net.fexcraft.mod.tpm.Reward;
-import net.minecraft.entity.player.EntityPlayer;
+import net.fexcraft.mod.uni.UniEntity;
 
 public interface RewardHandler {
 	
 	public static final TreeMap<String, RewardHandler> HANDLERS = new TreeMap<>();
 	public static final TreeMap<String, Reward> REWARDS = new TreeMap<>();
 	
-	public void rewardPlayer(EntityPlayer player, Reward reward);
+	public void rewardPlayer(UniEntity player, Reward reward);
 
 }
