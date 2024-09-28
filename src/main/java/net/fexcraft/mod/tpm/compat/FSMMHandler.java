cf9653e17420adeb2d00f8e3f377392320a94da9
Ferdinand Calo <fexcalo@gmail.com>
2024-09-28 10:10:03 +0200

    Update FSMMHandler.java

--- src/main/java/net/fexcraft/mod/tpm/compat/FSMMHandler.java
+++ src/main/java/net/fexcraft/mod/tpm/compat/FSMMHandler.java
@@ -1,24 +1,22 @@
 package net.fexcraft.mod.tpm.compat;
 
 import net.fexcraft.app.json.JsonMap;
-import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.fsmm.data.Manageable;
 import net.fexcraft.mod.fsmm.data.PlayerAccData;
 import net.fexcraft.mod.tpm.Reward;
 import net.fexcraft.mod.uni.UniEntity;
-import net.minecraft.entity.player.EntityPlayer;
 
 public class FSMMHandler {
 	
 	public static class Item implements RewardHandler {
 
 		@Override
-		public void rewardPlayer(EntityPlayer player, Reward reward){
+		public void rewardPlayer(UniEntity player, Reward reward){
 			JsonMap map = reward.reward;
-			PlayerAccData data = UniEntity.get(player).getApp(PlayerAccData.class);
+			PlayerAccData data = player.getApp(PlayerAccData.class);
 			data.addMoneyToInventory(map.get("amount", 0));
 			if(map.has("message")){
-				Print.chat(player, map.get("message").string_value());
+				player.entity.send(map.get("message").string_value());
 			}
 		}
 		
@@ -27,12 +25,12 @@ public class FSMMHandler {
 	public static class Currency implements RewardHandler {
 
 		@Override
-		public void rewardPlayer(EntityPlayer player, Reward reward){
+		public void rewardPlayer(UniEntity player, Reward reward){
 			JsonMap map = reward.reward;
-			PlayerAccData data = UniEntity.get(player).getApp(PlayerAccData.class);
+			PlayerAccData data = player.getApp(PlayerAccData.class);
 			data.getAccount().modifyBalance(Manageable.Action.ADD, map.getLong("amount", 0), UniEntity.getEntity(player));
 			if(map.has("message")){
-				Print.chat(player, map.get("message").string_value());
+				player.entity.send(map.get("message").string_value());
 			}
 		}
 		
